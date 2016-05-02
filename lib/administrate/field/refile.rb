69ac01a28adb2d7fb876300c04454676c8dc73c3
Adrian Rangel <adrian.rangel@gmail.com>
2016-05-01 20:23:48 -0500

    bump version

--- lib/administrate/field/refile.rb
+++ lib/administrate/field/refile.rb
@@ -4,7 +4,7 @@ require 'rails'
 module Administrate
   module Field
     class Refile < Administrate::Field::Base
-      VERSION = '0.0.3'
+      VERSION = '0.0.4'
       class Engine < ::Rails::Engine
       end
 
